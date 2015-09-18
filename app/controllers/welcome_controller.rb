e727acef8e74ca9439f574ac5979891d8fa97498
Kendall Park <darkidder@gmail.com>
2015-09-18 06:44:32 -0500

    added supplementary pharm

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,9 @@ class WelcomeController < ApplicationController
   def index
     @off_campus = params["off_campus"] == "true"
     @labs = YAML.load_file(Rails.root.join("config", "labs.yml"))
-    @pharm = YAML.load_file(Rails.root.join("config", "pharm.yml"))
+    pharm_cards = YAML.load_file(Rails.root.join("config", "pharm.yml"))
+    additional_pharm = YAML.load_file(Rails.root.join("config", "additional_pharm.yml"))
+    @pharm = pharm_cards.merge(additional_pharm)
   end
 
 end
